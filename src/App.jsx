4abaa8fbc991c8f89ee39657afd03522be23410d
bha8081 <bhaveshy2000r@gmail.com>
2024-04-20 22:40:20 +0530

    App file in TodoList

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { useDispatch } from 'react-redux';
+import AddTodo from './components/AddTodo/AddTodo'
+import TodoList from './components/TodoList/TodoList'
+import { bindActionCreators } from 'redux';
+import { addTodo, editTodo, deleteTodo, todoFinished } from "./slices/todoSlice";
+
+function App() {
+
+  const dispatch = useDispatch();
+  const actions = bindActionCreators({ addTodo, deleteTodo, editTodo, todoFinished }, dispatch);
+
+  return (
+        <>
+          <AddTodo addTodo={actions.addTodo} />
+          <TodoList deleteTodo={actions.deleteTodo} editTodo={actions.editTodo} todoFinished={actions.todoFinished} />
+        </>
+        
+  )
+}
+
+export default App;
